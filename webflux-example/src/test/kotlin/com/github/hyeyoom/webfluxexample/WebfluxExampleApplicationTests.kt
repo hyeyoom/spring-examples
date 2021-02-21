fdc761ca9bc03d6baa97e00ba8c8139ff92e1cf9
hyeyoom <neoul_chw@icloud.com>
2021-02-21 15:59:30 +0900

    

---
+++ webflux-example/src/test/kotlin/com/github/hyeyoom/webfluxexample/WebfluxExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.hyeyoom.webfluxexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
